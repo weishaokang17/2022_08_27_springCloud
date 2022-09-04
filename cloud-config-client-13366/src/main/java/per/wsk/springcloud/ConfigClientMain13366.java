1f79a3ca6863da3054541e4189af672a7740da81
weishaokang <weishaokang17@163.com>
2022-09-05 00:50:21 +0800

    springcloud学习

---
+++ cloud-config-client-13366/src/main/java/per/wsk/springcloud/ConfigClientMain13366.java
@@ -0,0 +1,14 @@
+package per.wsk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain13366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain13366.class,args);
+    }
+}
--- cloud-config-client-13366/src/main/java/per/wsk/springcloud/controller/ClientController.java
+++ cloud-config-client-13366/src/main/java/per/wsk/springcloud/controller/ClientController.java
@@ -6,9 +6,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
