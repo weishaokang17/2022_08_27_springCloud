152c1640225b89398c7eece1ff7a62f36f1b0e39
weishaokang <weishaokang17@163.com>
2022-09-06 16:29:59 +0800

    springcloud学习

--- cloud-sleuth-zipkin12003/src/main/java/per/wsk/springcloud/SleuthZipKin03Main.java
+++ cloud-sleuth-zipkin12003/src/main/java/per/wsk/springcloud/SleuthZipKin03Main.java
@@ -2,10 +2,11 @@ package per.wsk.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 @EnableFeignClients
 public class SleuthZipKin03Main {
