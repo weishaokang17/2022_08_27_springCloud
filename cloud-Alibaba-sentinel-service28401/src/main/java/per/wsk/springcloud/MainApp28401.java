1f4341564f3c1bd79fbc255ad8e1e60f98974ebe
weishaokang <weishaokang17@163.com>
2022-09-11 16:01:34 +0800

    springcloud学习

---
+++ cloud-Alibaba-sentinel-service28401/src/main/java/per/wsk/springcloud/MainApp28401.java
@@ -0,0 +1,16 @@
+package per.wsk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp28401 {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp28401.class, args);
+    }
+}
